544a9a74cdce7a6b09f06ca1cc0e0c242bd11105
enzoavalentim <enzo.valentim@estudante.ufla.br>
2025-06-24 14:03:41 -0300

    Add metodo para buscar top 5 repos

--- userData.py
+++ userData.py
@@ -1,7 +1,12 @@
 import requests
+from github import Github
+from collections import Counter, defaultdict
+from pydriller import Repository
+import os
+import git
+import shutil
 
 class UserData:
-
     def __init__(self, name, email, photo):
         self.name = name
         self.email = email
@@ -37,8 +42,8 @@ class UserData:
 
                 repo_name = repo["name"]
                 commits_url = f"https://api.github.com/repos/{userName}/{repo_name}/commits"
-
                 commits_response = requests.get(commits_url, headers=HEADERS)
+                
                 if commits_response.status_code == 200:
                     commits = commits_response.json()
                     for commit in commits:
@@ -46,13 +51,50 @@ class UserData:
                             commit_info = commit.get("commit", {}).get("author", {})
                             email = commit_info.get("email")
                             if email and "noreply.github.com" not in email:
-                                #print(f"E-mail encontrado nos commits: {email}")
                                 return email
 
         print("Nenhum e-mail público encontrado para este usuário.")
         return None
 
-
-
-
+    def getTop5Repos(self, github_token: str, username: str) -> dict:
+        g = Github(github_token)
+        user = g.get_user(username)
         
+        language_commit_counter = Counter()
+        repos_language = defaultdict(list)
+
+        for repo in user.get_repos():
+            try:
+                commits = repo.get_commits()
+                commit_count = commits.totalCount
+                langs = repo.get_languages()
+                repo_name = repo.name
+                repo_url = repo.clone_url
+
+                for lang in langs:
+                    language_commit_counter[lang] += commit_count
+                    repos_language[lang].append({
+                        'name': repo_name,
+                        'commits': commit_count,
+                        'locAdd': 0,
+                        'locRemov': 0,
+                        'url': repo_url
+                    })
+
+            except Exception as e:
+                print(f"⚠️ Erro ao acessar {repo.name}: {e}")
+
+        top_5_languages = language_commit_counter.most_common(5)
+        top5_repos_language = {lang: repos_language[lang] for lang, _ in top_5_languages}
+
+        return {
+            'user_info': {
+                'name': user.name,
+                'login': user.login,
+                'avatar_url': user.avatar_url,
+                'email': user.email
+            },
+            'top_languages': top_5_languages,
+            'repos_by_language': top5_repos_language
+        }
+    
