b8806d04ed4b8e0962f3c16392f4f22852c7b3ef
enzoavalentim <enzo.valentim@estudante.ufla.br>
2025-10-15 10:23:44 -0300

    Completando metodo uniqueImports com filtro das demais linguagens e adicionando metodo dropStandardlibs que retira bibliotecas padrão da lista de cada linguagem

--- Imports.py
+++ Imports.py
@@ -65,8 +65,56 @@ class Imports:
                     df = df[df['Imports'].notna()]        
                     df = df[df['Imports'].str.strip() != '']  
                     df = df.drop_duplicates(subset=['Imports']).reset_index(drop=True)
-                    
+
+                    df['Imports'] = df['Imports'].str.replace('import','').str.strip()
+                    df['Imports'] = df['Imports'].str.replace('#import','').str.strip()
+                    df['Imports'] = df['Imports'].str.replace('using','').str.strip()
+
                     df.to_excel(arquivo_path, index=False)
                     print(f"Arquivo processado: {arquivo_path.name}")
+
                 else:
                     print(f"A coluna 'Imports' não existe em {arquivo_path.name}")
+
+    @staticmethod
+    def dropStarndarLibs():
+
+        python = pd.read_excel("importTables/Python.xlsx") #Coluna X
+        java = pd.read_excel("importTables/Java.xlsx")
+        javascript = pd.read_excel("importTables/JavaScript.xlsx")
+        c = pd.read_excel("importTables/C.xlsx")
+        cpp = pd.read_excel("importTables/CPP.xlsx")
+        cs = pd.read_excel("importTables/CS.xlsx") 
+      
+
+        pythonStdLibs = pd.read_csv("standardLibs/standardLibsPython.csv") #Coluna Y
+        cStdLibs = pd.read_csv("standardLibs/standardLibsC.csv") #Coluna Y
+        cppStdLibs = pd.read_csv("standardLibs/standardLibsCPP.csv") #Coluna Y
+        csStdLibs = pd.read_csv("standardLibs/standardLibsCS.csv") #Coluna Y
+
+
+        #Python
+        dfFiltradoPython = python[~python['Imports'].isin(pythonStdLibs['Imports'])]
+        dfFiltradoPython.to_excel('importTables/Python.xlsx', index=False)
+
+        #Java
+        prefixosPadrao = ('java.', 'javax.', 'jdk.')
+        dfFiltradoJava = java[~java['Imports'].str.startswith(prefixosPadrao)]    
+        dfFiltradoJava.to_excel('importTables/Java.xlsx', index=False)
+
+        #JavaScript
+        #Bibliotecas padrão estão “embutidas” no ambiente de execução
+
+        #C
+        dfFiltradoC = c[~c['Imports'].isin(cStdLibs['Imports'])]
+        dfFiltradoC.to_excel('importTables/C.xlsx', index=False)
+
+        #C++
+        dfFiltradoCPP = cpp[~cpp['Imports'].isin(cppStdLibs['Imports'])]
+        dfFiltradoCPP.to_excel('importTables/CPP.xlsx', index=False)
+
+        #C#
+        dfFiltradoCs = cs[~cs['Imports'].isin(csStdLibs['Imports'])]
+        dfFiltradoCs.to_excel('importTables/CS.xlsx', index=False)
+
+        print("Linhas removidas com sucesso!")
