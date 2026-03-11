5a248d14da3a69979524d7c97cf8e375d7423926
enzoavalentim <enzo.valentim@estudante.ufla.br>
2026-03-11 14:29:32 -0300

    Alterando script.js para escrita de informações no dashboard

---
+++ Dashboard/script.js
@@ -0,0 +1,7 @@
+console.log("JS rodou");
+
+document.getElementById("dev-name").textContent = dados.dev_name;
+document.getElementById("dev-email").textContent = dados.email;
+document.getElementById("dev-photo").src = dados.photo;
+
+console.log("JS rodou");
