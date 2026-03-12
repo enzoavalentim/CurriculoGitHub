aedf51551c26169a2a34a37cf33a4e3fe59f67f3
enzoavalentim <enzo.valentim@estudante.ufla.br>
2026-03-12 19:34:08 -0300

    Alterando index.html icon-slot para mostrar os icones da linguagem principal do desenvolvedor utilizando o script.js, atualizando tamanho no style.css

--- Dashboard/script.js
+++ Dashboard/script.js
@@ -1,7 +1,15 @@
-console.log("JS rodou");
-
 document.getElementById("dev-name").textContent = dados.dev_name;
 document.getElementById("dev-email").textContent = dados.email;
 document.getElementById("dev-photo").src = dados.photo;
 
-console.log("JS rodou");
+const languageIcons = {
+  "Java": '<i class="devicon-java-plain-wordmark"></i>',
+  "JavaScript": '<i class="devicon-javascript-plain"></i>',
+  "Python": '<i class="devicon-python-plain-wordmark"></i>',
+  "C": '<i class="devicon-c-original"></i>',
+  "C++": '<i class="devicon-cplusplus-plain"></i>',
+  "C#": '<i class="devicon-csharp-plain"></i>'
+};
+
+document.getElementById("main-language-icon").innerHTML =
+  languageIcons[dados.main_language];
